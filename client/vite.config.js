d4871417c02faf8dab334ef3b77e667457a68486
Abhishek Shukla <4747uwu@gmail.com>
2026-04-16 12:13:54 +0530

    yay

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,4 +8,19 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: {
+        chrome: 109 << 16,
+      },
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
+    target: ['chrome109', 'edge109', 'firefox109', 'safari15'],
+  },
+  esbuild: {
+    target: 'chrome109',
+  },
 })
